a86efdb17bbac9a5de4fefd870171f745aacf36a
BugHunter-X <askzenai@gmail.com>
2025-01-12 09:34:06 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,17 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let numbers = [1, 2, 3, 4, 5]
+  let sortedNumbers = sort numbers
+  print sortedNumbers -- This will print [1,2,3,4,5]
+
+  let moreNumbers = [5,4,3,2,1]
+  let sortedMoreNumbers = sort moreNumbers
+  print sortedMoreNumbers --This will also print [1,2,3,4,5]
+
+  let stringNumbers = ["5","4","3","2","1"]
+  let sortedStringNumbers = sort stringNumbers
+  print sortedStringNumbers -- This will print ["1","2","3","4","5"]
+```
